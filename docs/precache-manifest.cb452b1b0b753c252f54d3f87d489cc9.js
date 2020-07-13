2c125b588860ce10519ee6fb07a28fc9cd2c8951
Javier Serna <javiserna7@gmail.com>
2020-07-13 17:45:15 +0200

    docs agregados

---
+++ docs/precache-manifest.cb452b1b0b753c252f54d3f87d489cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9998373dc78181225a9a147a721c1292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec50246c15619019e48a",
+    "url": "/static/css/main.7b06d3cd.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec50246c15619019e48a",
+    "url": "/static/js/main.abe333f6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
